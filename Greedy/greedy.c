0eb67691dd5856d5ad9b935340f1bfc9e2030136
DhyanShah22 <dhyanshah229@gmail.com>
2024-03-15 09:49:42 +0530

    Greedy

--- Greedy/greedy.c
+++ Greedy/greedy.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure to represent an event
 typedef struct {
     char name;
     int start;
@@ -9,30 +8,23 @@ typedef struct {
     int weight;
 } Event;
 
-// Function to compare events by their ending time (used for sorting)
 int compare(const void *a, const void *b) {
     return (((Event*)a)->end - ((Event*)b)->end);
 }
 
-// Function to find the maximum number of events that can be scheduled
 int maxEvents(Event events[], int n, int *maxWeight) {
-    // Sort the events based on their ending times
     qsort(events, n, sizeof(Event), compare);
     
-    // Initialize an array to store the maximum number of events that can be scheduled
     int *dp = (int*)malloc(n * sizeof(int));
     int *weights = (int*)malloc(n * sizeof(int));
     
-    // Base case
-    dp[0] = 1; // The first event can always be scheduled
+    dp[0] = 1; 
     weights[0] = events[0].weight;
     
-    // Dynamic programming to compute the maximum number of events that can be scheduled
     for (int i = 1; i < n; i++) {
-        dp[i] = 1; // Initialize the maximum number of events that can be scheduled with the current event
+        dp[i] = 1; 
         weights[i] = events[i].weight;
         
-        // Iterate through previous events to find the maximum number of events that can be scheduled with the current event
         for (int j = 0; j < i; j++) {
             if (events[i].start >= events[j].end) {
                 if (dp[i] < dp[j] + 1) {
@@ -43,7 +35,6 @@ int maxEvents(Event events[], int n, int *maxWeight) {
         }
     }
     
-    // Find the maximum number of events that can be scheduled
     int max = 0;
     *maxWeight = 0;
     for (int i = 0; i < n; i++) {
@@ -53,7 +44,6 @@ int maxEvents(Event events[], int n, int *maxWeight) {
         }
     }
     
-    // Free dynamically allocated memory
     free(dp);
     free(weights);
     
@@ -61,29 +51,26 @@ int maxEvents(Event events[], int n, int *maxWeight) {
 }
 
 int main() {
+    printf("Dhyan Shah\n");
+    printf("22BCP269\n");
     int n;
     printf("Enter the number of events: ");
     scanf("%d", &n);
     
-    // Allocate memory for events
     Event *events = (Event*)malloc(n * sizeof(Event));
     
-    // Input events
     printf("Enter the events:\n");
     for (int i = 0; i < n; i++) {
         printf("Event %d (Name Start End Weight): ", i + 1);
         scanf(" %c %d %d %d", &events[i].name, &events[i].start, &events[i].end, &events[i].weight);
     }
     
-    // Calculate the maximum number of events that can be scheduled
     int maxWeight;
     int max = maxEvents(events, n, &maxWeight);
     
-    // Print the result
     printf("Maximum number of events that can be scheduled: %d\n", max);
     printf("Maximum weight when all events are scheduled: %d\n", maxWeight);
     
-    // Free dynamically allocated memory
     free(events);
     
     return 0;
